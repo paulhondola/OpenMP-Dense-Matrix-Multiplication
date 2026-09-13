d8130f32e8fe63ca4faf93a211d52977864c5e97
Paul Hondola <paulhondola@gmail.com>
2026-09-13 23:16:53 +0300

    Update tiled benchmark results and plots

--- benchmark/src/tiled/parallel/mm_tiled_parallel.c
+++ benchmark/src/tiled/parallel/mm_tiled_parallel.c
@@ -19,16 +19,15 @@ double parallel_multiply_tiled_tasks(const Matrix *restrict a,
 #pragma omp single
     for (block_i = 0; block_i < n; block_i += block_size) {
       for (block_j = 0; block_j < n; block_j += block_size) {
+#pragma omp task
         for (block_k = 0; block_k < n; block_k += block_size) {
           int i_end = (block_i + block_size > n) ? n : (block_i + block_size);
           int j_end = (block_j + block_size > n) ? n : (block_j + block_size);
           int k_end = (block_k + block_size > n) ? n : (block_k + block_size);
-#pragma omp task
           for (i = block_i; i < i_end; i++) {
             for (k = block_k; k < k_end; k++) {
               temp = a->data[i][k];
               for (j = block_j; j < j_end; j++) {
-#pragma omp atomic
                 c->data[i][j] += temp * b->data[k][j];
               }
             }
